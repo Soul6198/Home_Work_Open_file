089c6433ba47331b6b3e98b06c815805678a1cb4
Alexandr_Popov <Reorin262@gmail.com>
2022-07-22 22:26:24 +0300

    2 Задание

--- Open_Fail.py
+++ Open_Fail.py
@@ -20,3 +20,19 @@ def file_read(file_name):
             file.readline()
     return book
 pprint(file_read(file_name))
+print()
+
+print('2 задание')
+def get_shop_list_by_dishes(dishes, pers_c):
+    dishes = ['Запеченный картофель', 'Омлет']
+    pers_c = 2
+    recipes_dict = {}
+    for items in dishes:
+        for keys, values in file_read(file_name).items():
+            if keys == items:
+                for elements in values:
+                  elements['quantity'] = float(elements['quantity']) * pers_c   
+    return pprint(get_shop_list_by_dishes(dishes, pers_c))
+print()
+
+print('Задание 3')
